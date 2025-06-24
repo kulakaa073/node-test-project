231995e0e09fb61fd3c4f89615a8823ac6839eac
Shade <kulakaa073@gmail.com>
2025-06-24 12:51:50 +0300

    logging and cors

--- src/index.js
+++ src/index.js
@@ -1,18 +1,44 @@
 // src/index.js
 
 import express from 'express';
+import pino from 'pino-http';
+import cors from 'cors';
 
 const app = express();
 
 const PORT = 3000;
-app.use((req, res, next) => {
-  console.log(`Time: ${new Date().toLocaleString()}`);
-  next();
-});
+// order is important here, call are synchronous
+// app.use((req, res, next) => {
+//   console.log(`Time: ${new Date().toLocaleString()}`);
+//   next();
+// });
+
+app.use(cors());
+
+app.use(
+  pino({
+    transport: {
+      target: 'pino-pretty',
+    },
+  })
+);
 
 app.get('/', (req, res) => {
   res.json({
     message: 'Hello world!',
+  });
+});
+
+app.use('*', (req, res, next) => {
+  res.status(404).json({
+    message: 'Route not found',
+  });
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    message: 'Something went wrong',
+    error: err.message,
   });
 });
 
