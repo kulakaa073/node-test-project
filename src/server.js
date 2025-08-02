fba2de727216058d4a8bbd98677d3897882c1516
Shade <kulakaa073@gmail.com>
2025-08-02 22:24:59 +0300

    trying swagger

--- src/server.js
+++ src/server.js
@@ -14,6 +14,8 @@ import cookieParser from 'cookie-parser';
 
 import { UPLOAD_DIR } from './constants/index.js';
 
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
+
 const PORT = Number(getEnvVar('PORT', '3000'));
 
 export const startServer = () => {
@@ -40,6 +42,8 @@ export const startServer = () => {
   app.use(router);
 
   app.use('/uploads', express.static(UPLOAD_DIR));
+
+  app.use('/api-docs', swaggerDocs());
 
   app.use('*', notFoundHandler);
 
